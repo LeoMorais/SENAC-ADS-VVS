ac41e560fee17b16311c90e615f5e2d8beb73a04
Leonardo Pereira de Morais <leonardopmorais@outlook.com.br>
2020-05-08 18:03:32 -0300

    Update Soma.java

--- JUnitteste/src/com/leonardo/Soma.java
+++ JUnitteste/src/com/leonardo/Soma.java
@@ -2,9 +2,9 @@ package com.leonardo;
 
 public class Soma {
 	
-	//Método para operação de soma
+	//MÃ©todo para operaÃ§Ã£o de soma
 	public int soma(int n1, int n2) {
-		return n1 + n2;
+		return n1 - n2;
 	}
 	
 }
